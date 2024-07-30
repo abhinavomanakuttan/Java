e1ea4935c86433f84ac18a41d04e51e47300f59e
ABHINAV OMANAKUTTAN <123224545+abhinavomanakuttan@users.noreply.github.com>
2024-07-30 21:38:54 +0530

    Add files via upload

---
+++ 04_Loops/01_basic_for_loop.java
@@ -0,0 +1,27 @@
+class basic_for_loop{
+    public static void main(String[] args) {
+        /*
+         Syntax of for loops:
+            for(inirilasation ;condition ;increment/decrement){
+                    //body
+            
+            }
+         */
+        //print number from 1 to 5 
+
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+
+        System.err.println("");
+
+        //to print hello fro 5 times
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hello ");
+            
+        }
+
+       
+    }
+}
---
+++ 04_Loops/03_basic_whlie_loop.java
@@ -0,0 +1,32 @@
+ class basic_whlie_loop {
+    public static void main(String[] args) {
+    
+     
+        
+        //while loop
+        /*
+        Syntax
+           
+            while(condion){
+
+                //body 
+
+            }
+
+         */
+        int i=1;
+        while(i<=5){
+            System.out.println("HELLO");
+            i++;
+        }
+
+        //NOTE
+        /*while loop is basicly using when we don't know 
+            how many time the loop is going to run
+                       */
+
+    }
+
+    
+    
+}
